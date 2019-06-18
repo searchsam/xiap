54bd6fbb8fb0f6b1f98852dba0d0eef5901dd719
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-06-17 18:02:46 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.route('/data/<string:serialnum>/<string:uuid>')
+def getData(serialnum, uuid):
+    data = dict()
+    data['serialnum'] = serialnum
+    data['uuid'] = uuid
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
+    return 'Serial Number: {0} <br> UUID: {1}'.format(serialnum, uuid)
